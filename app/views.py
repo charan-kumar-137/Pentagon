8b73f44f0c5cb38c64412f7ae491cb5fa3af38d6
charan-kumar-137 <charankumar137@gmail.com>
2020-07-11 19:39:11 +0530

    Created Home Page

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request,'home.html')
 
 
 def profile(request):
